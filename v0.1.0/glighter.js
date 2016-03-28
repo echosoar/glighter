ad639e9b75d425da604049c7fd6bf8907fd149eb
echosoar <82163514@qq.com>
2016-03-28 23:13:06 +0800

    update@160328

--- v0.1.0/glighter.js
+++ v0.1.0/glighter.js
@@ -6,18 +6,21 @@ Website : http://iwenku.net
 */
 (function(obj,undefined){
 	if(obj.glighter!=null)return;
-	var codeData;//代码数据
-	
-	var dataStatus;//行数据状态
+	var codeData;
+	var dataStatus;
 	var glighterObj={};
-	var supportLanguage="|html|";
+	var supportLanguage="|html|javascript|css|";
 	
 	var initExp=[/\t/g,/ /g,/\n/g];
 	var initExpTo=["#GLighter_T#","#GLighter_Space#","#GLighter_N#"];
 	
 	var languageCommentsExp={
 		html:[/(<|&lt;)!\-\-(.*?)\-\-(>|&gt;)/g],
-		htmlTo:["#GLighter_Comments#GLighter_Comments$1!--$2--$3#GLighter_Comments#"]
+		htmlTo:["#GLighter_Comments#GLighter_Comments$1!--$2--$3#GLighter_Comments#"],
+		css:[/(\/\*)(.*?)(\*\/)/g],
+		cssTo:["#GLighter_Comments#GLighter_Comments$1$2$3#GLighter_Comments#"],
+		javascript:[/(\/\*)(.*?)(\*\/)/g],
+		javascriptTo:["#GLighter_Comments#GLighter_Comments$1$2$3#GLighter_Comments#"]
 	};
 	
 	var languageExp={
@@ -27,8 +30,26 @@ Website : http://iwenku.net
 			],
 		htmlTo:[
 			'<span class="glighter_common_tag">&lt;$1$2&gt;</span>',
-			'<span class="glighter_common_tag">&lt;$1</span>#GLighter_Space#$2<span class="glighter_common_tag">$3&gt;</span>',
-			]
+			'<span class="glighter_common_tag">&lt;$1</span>#GLighter_Space#$2<span class="glighter_common_tag">$3&gt;</span>'
+			],
+		css:[
+			/(#GLighter_Space#|#GLighter_T#|;)([\w-_]+)((?:#GLighter_Space#|#GLighter_T#)*):((?:#GLighter_Space#|#GLighter_T#)*)(#(?:[0-9a-fA-F]{3,6}))((?:#GLighter_Space#|#GLighter_T#)*);/ig,
+			/(#GLighter_Space#|#GLighter_T#|;)([\w-_]+)((?:#GLighter_Space#|#GLighter_T#)*):((?:#GLighter_Space#|#GLighter_T#)*)([\w-_]+)((?:#GLighter_Space#|#GLighter_T#)*);/ig
+		],
+		cssTo:[
+			'$1<span class="glighter_property">$2</span>$3 : $4<span class="glighter_string" style="color:$5;text-shadow:1px 1px 1px #000;">$5</span>$6;',
+			'$1<span class="glighter_property">$2</span>$3 : $4<span class="glighter_string">$5</span>$6;'
+		],
+		javascript:[
+			/([^\w])(function|return|var|false|true|alert|if|else|elseif|undefined|typeof|while|for|in)([^\w])/ig,
+			/([\w_]+)(\s*)\((.*?)\)/ig,
+			/\.([\w_]+)([\.;\s])/ig
+			],
+		javascriptTo:[
+			'$1<span class="glighter_common_tag">$2</span>$3',
+			'<span class="glighter_function">$1</span>$2($3)',
+			'.<span class="glighter_string">$1</span>$2'
+		]
 	};
 
 	var isHavePropertyHTMLExp=/&lt;(?:\w*?)(?:#GLighter_Space#|#GLighter_T#){1,}(.*?)(?:[\/]{0,1})&gt;/g;
@@ -37,13 +58,16 @@ Website : http://iwenku.net
 		if(options.target==null)return;
 
 		glighterObj.target=options.target;
-		glighterObj.language=options.language.toLowerCase()||"html";
+		glighterObj.languageStr=options.language.toLowerCase()||"html";
 		glighterObj.dark=options.dark||true;
 		
 		
 		/*unsupport this language*/
-		if(supportLanguage.indexOf("|"+glighterObj.language+"|")==-1){
-			return;
+		glighterObj.language=glighterObj.languageStr.split("|");
+		for(lang in glighterObj.language){
+			if(supportLanguage.indexOf("|"+glighterObj.language[lang]+"|")==-1){
+				return;
+			}
 		}
 		
 		codeData=glighterObj.target.innerHTML;
@@ -54,7 +78,7 @@ Website : http://iwenku.net
 		
 		commentSplit();//注释分离
 		
-		if(glighterObj.language==="html"){
+		if(/html/.test(glighterObj.languageStr)){
 			htmlTagPropertyExec();
 		}
 		
@@ -70,9 +94,11 @@ Website : http://iwenku.net
 	}
 	
 	function languageCommentsExec(){
-		languageCommentsExp[glighterObj.language].forEach(function(reg,index){
-			codeData=codeData.replace(reg,languageCommentsExp[glighterObj.language+"To"][index]);
-		});
+		for(var language in glighterObj.language){
+			languageCommentsExp[glighterObj.language[language]].forEach(function(reg,index){
+				codeData=codeData.replace(reg,languageCommentsExp[glighterObj.language[language]+"To"][index]);
+			});
+		}
 	}
 	
 	function commentSplit(){
@@ -85,20 +111,22 @@ Website : http://iwenku.net
 	}
 	
 	function languageExec(){
-		languageExp[glighterObj.language].forEach(function(reg,index){
-			dataStatus.forEach(function(arr,indexs){
-				if(!arr[1]){
-					dataStatus[indexs][0]=arr[0].replace(reg,languageExp[glighterObj.language+"To"][index]);
-				}
+		for(var language in glighterObj.language){
+			languageExp[glighterObj.language[language]].forEach(function(reg,index){
+				dataStatus.forEach(function(arr,indexs){
+					if(!arr[1]){
+						dataStatus[indexs][0]=arr[0].replace(reg,languageExp[glighterObj.language[language]+"To"][index]);
+					}
+				});
 			});
-		});
+		}
 	}
 	
 	function htmlTagPropertyExec(){
 		dataStatus.forEach(function(arr,indexs){
 			if(!arr[1]){
 				if(isHavePropertyHTMLExp.test(arr[0])){
-					arr[0]=arr[0].replace(/(#GLighter_Space#|#GLighter_T#){2,}/g,"#GLighter_Space#")
+					//arr[0]=arr[0].replace(/(#GLighter_Space#|#GLighter_T#){2,}/g,"#GLighter_Space#")
 					var htmlPropertyReg=/&lt;(?:\w+?)#GLighter_Space#([\w-_]+=.*?)(?:\/*)&gt;/g;
 					var temPropertyLine;
 					while(temPropertyLine=htmlPropertyReg.exec(arr[0])){
@@ -118,7 +146,7 @@ Website : http://iwenku.net
 		for(var chunk in dataStatus){
 			var temdata=dataStatus[chunk][0].replace(/#GLighter_T#/g,'<span class="glighter_space4"></span>').replace(/#GLighter_Space#/g,'<span class="glighter_space1"></span>');
 			if(dataStatus[chunk][1]){
-				temResStr+='<p class="glighter_comment">'+temdata.replace(/#GLighter_N#/g,'</p>#GLighter_N#<p class="glighter_comment">')+'</p>';
+				temResStr+='<span class="glighter_comment">'+temdata.replace(/#GLighter_N#/g,'</span>#GLighter_N#<span class="glighter_comment">')+'</span>';
 			}else{
 				temResStr+=temdata;
 			}
